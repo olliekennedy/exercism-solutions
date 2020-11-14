005dcfae3b0ac68bf8ad6ee0aa5dcf2e822cffef
olliekennedy <ollie.kennedy1@gmail.com>
2020-11-14 15:42:31 +0000

    triangle tests passing

--- javascript/triangle/triangle.js
+++ javascript/triangle/triangle.js
@@ -1,22 +1,26 @@
-//
-// This is only a SKELETON file for the 'Triangle' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
-
 export class Triangle {
-  constructor() {
-    throw new Error("Remove this statement and implement this function");
+  constructor(side1, side2, side3) {
+    this.sides = [side1, side2, side3];
   }
 
   isEquilateral() {
-    throw new Error("Remove this statement and implement this function");
+    if (this.isNotTriangle()) return false
+    return (this.sides[0] == this.sides[1]) && (this.sides[1] == this.sides[2]);
   }
 
   isIsosceles() {
-    throw new Error("Remove this statement and implement this function");
+    if (this.isNotTriangle()) return false;
+    if ((this.sides[0] == this.sides[1]) || (this.sides[1] == this.sides[2]) || (this.sides[0] == this.sides[2])) return true;
+    return false;
   }
 
   isScalene() {
-    throw new Error("Remove this statement and implement this function");
+    return (!(this.isNotTriangle()) && !this.isIsosceles() && !this.isEquilateral());
+  }
+
+  isNotTriangle() {
+    if (((this.sides[0] + this.sides[1]) < this.sides[2]) || ((this.sides[1] + this.sides[2]) < this.sides[0]) || ((this.sides[2] + this.sides[0]) < this.sides[1])) return true
+    return ((this.sides[0] == 0) || (this.sides[1] == 0) || (this.sides[2] == 0));
+
   }
 }

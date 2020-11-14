d076116689f1c7708ce62be4200cf01a2a965843
olliekennedy <ollie.kennedy1@gmail.com>
2020-11-14 13:47:19 +0000

    refactor leap after inspiration

--- javascript/leap/leap.js
+++ javascript/leap/leap.js
@@ -3,9 +3,5 @@
 // convenience to get you started writing code faster.
 //
 
-export const isLeap = (year) => {
-  if (year % 400 == 0) return true
-  if (year % 100 == 0) return false
-  if (year % 4 == 0) return true
-  return false;
-};
+export const isLeap = (year) =>
+  ((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0);

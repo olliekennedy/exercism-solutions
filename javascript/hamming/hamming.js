a4dd5f261f4d742110ec6791cb4060106159d10d
olliekennedy <ollie.kennedy1@gmail.com>
2020-11-16 09:22:00 +0000

    refactor hamming

--- javascript/hamming/hamming.js
+++ javascript/hamming/hamming.js
@@ -4,12 +4,14 @@
 //
 
 export const compute = (l,r) => {
-  if (l.length == 0 && r.length > 0) throw Error('left strand must not be empty');
-  if (r.length == 0 && l.length > 0) throw Error('right strand must not be empty');
-  if (l.length != r.length) throw Error('left and right strands must be of equal length');
+  if (l.length != r.length) {
+    if (l.length == 0) throw Error('left strand must not be empty');
+    if (r.length == 0) throw Error('right strand must not be empty');
+    throw Error('left and right strands must be of equal length');
+  }
   var ham = 0
   for (var i = 0; i < l.length; i++) {
-    if (l[i] != r[i]) ham ++
+    if (l[i] != r[i]) ham++
   }
   return ham
 };

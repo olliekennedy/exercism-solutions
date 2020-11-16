0df645c258df81e8d2e2d35fee0c476e69667914
olliekennedy <ollie.kennedy1@gmail.com>
2020-11-16 08:23:30 +0000

    refactor clock

--- javascript/clock/clock.js
+++ javascript/clock/clock.js
@@ -1,29 +1,15 @@
-//
-// This is only a SKELETON file for the 'Clock' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
-
 export class Clock {
   constructor(hours, minutes=0) {
-    this.d = new Date(2020, 11, 11, hours, minutes, 0, 0)
-  }
-
-  padded(int) {
-    return ('' + int).length == 2 ? '' + int : '0' + int;
+    this.d = new Date((hours*60 + minutes) * 60000)
   }
 
   toString() {
-    // console.log(this.d)
-    var h = this.padded(this.d.getHours());
-    var m = this.padded(this.d.getMinutes());
-    return `${h}:${m}`;
+    return this.d.toISOString().substr(11, 5);
   }
 
   plus(int) {
     this.d = new Date(this.d.getTime() + int*60000);
     return this;
-    // return this.d
-    // return this.d.setMinutes((this.d.getMinutes() + int)) ;
   }
 
   minus(int) {
@@ -32,6 +18,6 @@ export class Clock {
   }
 
   equals(clock) {
-    return this.d.toISOString().substr(11, 8) == clock.d.toISOString().substr(11, 8);;
+    return this.toString() == clock.toString();
   }
 }

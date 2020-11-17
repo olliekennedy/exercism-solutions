6c992e91330b792bff11eb138dccec107f918e91
olliekennedy <ollie.kennedy1@gmail.com>
2020-11-17 09:02:49 +0000

    complete and refactor pangram

--- javascript/pangram/pangram.js
+++ javascript/pangram/pangram.js
@@ -1,13 +1,3 @@
-//
-// This is only a SKELETON file for the 'Pangram' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
-
-export const isPangram = (str) => {
-  var alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
-  var str_chars = str.toLowerCase().split('');
-  alphabet.forEach(char => {
-    if (str_chars.includes(char) == false) return false;
-  })
-  return str_chars.length > 1
+export const isPangram = (str, char = '') => {
+  return 'abcdefghijklmnopqrstuvwxyz'.split('').every(function(char) {return str.toLowerCase().includes(char)}, char)
 };

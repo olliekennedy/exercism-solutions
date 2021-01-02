151d708444eb66b7fe941fe6a85c095bf7d12094
Ollie Kennedy <ollie.kennedy1@gmail.com>
2021-01-02 10:20:50 +0000

    complete armstrong numbers

--- java/armstrong-numbers/src/main/java/ArmstrongNumbers.java
+++ java/armstrong-numbers/src/main/java/ArmstrongNumbers.java
@@ -1,18 +1,12 @@
 class ArmstrongNumbers {
 
     boolean isArmstrongNumber(int numberToCheck) {
-
-      String[] input = String.valueOf(numberToCheck).split("");
+      char[] input = String.valueOf(numberToCheck).toCharArray();
       int sum = 0;
-      for (var i = 0; i < input.length; i++) {
-        sum = sum + (int)Math.pow(Integer.valueOf(input[i]), input.length);
+      for (int i = 0; i < input.length; i++) {
+        sum = sum + (int)Math.pow(Character.getNumericValue(input[i]), input.length);
       }
-      if (sum == Integer.valueOf(numberToCheck)) {
-        return true;
-      } else {
-        return false;
-      }
-
+      return sum == numberToCheck;
     }
 
 }

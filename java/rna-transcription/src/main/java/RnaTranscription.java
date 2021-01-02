c6b9da29ebdcd2e5735ae1224c7c75c1f647407d
Ollie Kennedy <ollie.kennedy1@gmail.com>
2021-01-02 10:53:26 +0000

    complete rna transcription

--- java/rna-transcription/src/main/java/RnaTranscription.java
+++ java/rna-transcription/src/main/java/RnaTranscription.java
@@ -1,7 +1,22 @@
+import java.util.HashMap;
+
 class RnaTranscription {
 
     String transcribe(String dnaStrand) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+      HashMap<Character, Character> pairs = new HashMap<Character, Character>();
+      pairs.put('C', 'G');
+      pairs.put('G', 'C');
+      pairs.put('T', 'A');
+      pairs.put('A', 'U');
+      if (dnaStrand.length() == 0) {
+        return "";
+      }
+      char[] nucleotides = dnaStrand.toCharArray();
+      char[] output = new char[nucleotides.length];
+      for (var i = 0; i < nucleotides.length; i++) {
+        output[i] = pairs.get(nucleotides[i]);
+      }
+      return new String(output);
     }
 
 }

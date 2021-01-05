1bd79bdfc2104ff08210b595f6b0a7605b19919e
Ollie Kennedy <ollie.kennedy1@gmail.com>
2021-01-05 16:05:35 +0000

    complete grains

--- java/grains/src/main/java/Grains.java
+++ java/grains/src/main/java/Grains.java
@@ -3,11 +3,22 @@ import java.math.BigInteger;
 class Grains {
 
     BigInteger grainsOnSquare(final int square) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        if (square < 1 || square > 64) {
+          throw new IllegalArgumentException("square must be between 1 and 64");
+        }
+        BigInteger two = new BigInteger("2");
+        return two.pow(square - 1);
     }
 
     BigInteger grainsOnBoard() {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+      BigInteger total = new BigInteger("0");
+      for (int i = 1; i < 65; i++) {
+        total = total.add(grainsOnSquare(i));
+      }
+      return total;
+
+      // BigInteger total = grainsOnSquare(65).subtract(new BigInteger("1"));
+      // return total;
     }
 
 }

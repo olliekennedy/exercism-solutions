8695690f88732c8be351dfabccf2c0bfbbbecf91
Ollie Kennedy <ollie.kennedy1@gmail.com>
2021-01-08 13:47:00 +0000

    complete hamming

--- java/hamming/src/main/java/Hamming.java
+++ java/hamming/src/main/java/Hamming.java
@@ -1,9 +1,26 @@
 public class Hamming {
+    char[] l, r;
     public Hamming(String leftStrand, String rightStrand) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+      if (leftStrand.length() != rightStrand.length()) {
+        if (leftStrand.length() == 0) {
+          throw new IllegalArgumentException("left strand must not be empty.");
+        } else if (rightStrand.length() == 0) {
+          throw new IllegalArgumentException("right strand must not be empty.");
+        } else {
+        throw new IllegalArgumentException("leftStrand and rightStrand must be of equal length.");
+        }
+      }
+      l = leftStrand.toCharArray();
+      r = rightStrand.toCharArray();
     }
 
     public int getHammingDistance() {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+      int total = 0;
+      for (int i = 0; i < l.length; i++) {
+        if (l[i] != r[i]) {
+          total += 1;
+        }
+      }
+      return total;
     }
 }

c4af5898ac117087823d322150f1a7bbd3add83c
Ollie Kennedy <ollie.kennedy1@gmail.com>
2021-01-08 13:27:57 +0000

    complete pangram

--- java/pangram/src/main/java/PangramChecker.java
+++ java/pangram/src/main/java/PangramChecker.java
@@ -1,7 +1,21 @@
 public class PangramChecker {
 
     public boolean isPangram(String input) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+      char[] letters = "abcdefghijklmnopqrstuvwxyz".toCharArray();
+      char[] inputChars = input.toCharArray();
+      for (int i = 0; i < inputChars.length; i++) {
+        for (int j = 0; j < letters.length; j++) {
+          if (Character.toLowerCase(inputChars[i]) == letters[j]) {
+            letters[j] = '0';
+          }
+        }
+      }
+      for (int i = 0; i < letters.length; i++) {
+        if (letters[i] != '0') {
+          return false;
+        }
+      }
+      return true;
     }
 
 }
